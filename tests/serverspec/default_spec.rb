aa63defd613fc2fc8893d26ac04ac8cb07a14857
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-09 11:20:01 +0700

    Support other platforms (#4)
    * initial support of Ubuntu
    
    * initial support of OpenBSD
    
    * update meta
    
    * s/nfsd_exports_file/__nfsd_exports_file/
    
    * add a handler to debug build in Jenkins
    
    * add a comment why `rc_stop` is `NO`
    
    it is deliverately disabled
    
    * try `retries`
    
    * fix `until`
    
    * document `retries`
    
    in Jenkins job, the handler fails a few times, succeeds after second or
    third try.
    
    * use `Restart mountd OpenBSD`
    
    and put the comment in the right place.
    https://github.com/reallyenglish/ansible-role-nfs-server/pull/4#discussion_r121040226
    
    * fix wrong merge
    
    * switch to master
    
    the branch has gone

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -10,6 +10,8 @@ default_group = "root"
 case os[:family]
 when "freebsd"
   default_group = "wheel"
+when "openbsd"
+  default_group = "wheel"
 end
 
 describe file(exports) do
@@ -18,10 +20,90 @@ describe file(exports) do
   it { should be_mode 644 }
   it { should be_owned_by default_user }
   it { should be_grouped_into default_group }
-  its(:content) { should match(/#{Regexp.escape("/usr/local")}\s+#{Regexp.escape("-network 192.168.1.0/24")}/) }
+  case os[:family]
+  when "freebsd", "openbsd"
+    its(:content) { should match(/#{Regexp.escape("/usr/local")}\s+#{Regexp.escape("-network 192.168.1.0/24")}/) }
+  else
+    its(:content) { should match(/#{Regexp.escape("/usr/local")}\s+#{Regexp.escape("192.168.1.0/24")}/) }
+  end
 end
 
 case os[:family]
+when "openbsd"
+  describe service("nfsd") do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe service("portmap") do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe service("mountd") do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe command("rpcinfo -p localhost") do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match(/portmapper/) }
+    its(:stdout) { should match(/nfs/) }
+    its(:stdout) { should match(/mountd/) }
+  end
+when "ubuntu"
+  describe package("nfs-kernel-server") do
+    it { should be_installed }
+  end
+  describe file("/etc/default/nfs-common") do
+    it { should exist }
+    it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+    its(:content) { should match(/^STATDOPTS=""$/) }
+    its(:content) { should match(/^NEED_GSSD=""$/) }
+  end
+
+  describe file("/etc/default/nfs-kernel-server") do
+    it { should exist }
+    it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+    its(:content) { should match(/^RPCNFSDCOUNT="4"$/) }
+    its(:content) { should match(/^RPCNFSDPRIORITY="0"$/) }
+    its(:content) { should match(/^RPCMOUNTDOPTS="--manage-gids"$/) }
+    its(:content) { should match(/^NEED_SVCGSSD=""$/) }
+    its(:content) { should match(/^RPCSVCGSSDOPTS=""$/) }
+    its(:content) { should match(/^RPCNFSDOPTS=""$/) }
+  end
+
+  if os[:release].to_f >= 16.04
+    describe service("nfs-mountd") do
+      it { should be_running }
+      it { should be_enabled }
+    end
+  else
+    describe command("service nfs-kernel-server status") do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match(/nfsd running/) }
+      its(:stderr) { should eq "" }
+    end
+
+    describe process("rpc.mountd") do
+      it { should be_running }
+    end
+
+    describe command("rpcinfo -s  | awk '{ print $4 }'") do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match(/portmapper/) }
+      its(:stdout) { should match(/nlockmgr/) }
+      its(:stdout) { should match(/mountd/) }
+      its(:stdout) { should match(/nfs/) }
+      its(:stderr) { should eq "" }
+    end
+  end
 when "freebsd"
   describe file("/etc/rc.conf.d/mountd") do
     it { should exist }
